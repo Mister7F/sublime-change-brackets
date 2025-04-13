c55b9512a29cb7e3ef9c3c047d66759f3b385069
Mister7F <mister7f@gmail.com>
2025-04-13 17:47:56 +0200

    [IMP] make it work inside and around the selection

--- change_brackets.py
+++ change_brackets.py
@@ -1,8 +1,8 @@
 import sublime
 import sublime_plugin
 
+brackets = ("()", "{}", "[]", "''", '""')
 
-brackets = ["()", "{}", "[]", "''", '""']
 
 class ChangeBracketsCommand(sublime_plugin.TextCommand):
     """Change the quote / brackets around the selection."""
@@ -20,19 +20,30 @@ class ChangeBracketsCommand(sublime_plugin.TextCommand):
 
         self.view.run_command("change_brackets_at", {"bracket": brackets[option]})
 
-class ChangeBracketsAtCommand(sublime_plugin.TextCommand):
 
+class ChangeBracketsAtCommand(sublime_plugin.TextCommand):
     def run(self, edit, bracket):
         if not self.view.sel():
             return
 
         for sel in list(self.view.sel()):
             a, b = sorted(sel.to_tuple())
+
+            # First try to change the bracket inside the selection
+            b1 = sublime.Region(a, a + 1)
+            b2 = sublime.Region(b - 1, b)
+            br = self.view.substr(b1) + self.view.substr(b2)
+
+            if br in brackets:
+                self.view.replace(edit, b1, bracket[0])
+                self.view.replace(edit, b2, bracket[1])
+                continue
+
+            # Otherwise, look around
             b1 = sublime.Region(a - 1, a)
             b2 = sublime.Region(b, b + 1)
             br = self.view.substr(b1) + self.view.substr(b2)
-            if br not in brackets:
+            if br in brackets:
+                self.view.replace(edit, b1, bracket[0])
+                self.view.replace(edit, b2, bracket[1])
                 continue
-
-            self.view.replace(edit, b1, bracket[0])
-            self.view.replace(edit, b2, bracket[1])
