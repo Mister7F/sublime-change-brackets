8ee5c7d4c568ce500ee9c4ae1f4c0e24b47576f3
Mister7F <mister7f@gmail.com>
2025-04-13 17:39:28 +0200

    Init

---
+++ change_brackets.py
@@ -0,0 +1,38 @@
+import sublime
+import sublime_plugin
+
+
+brackets = ["()", "{}", "[]", "''", '""']
+
+class ChangeBracketsCommand(sublime_plugin.TextCommand):
+    """Change the quote / brackets around the selection."""
+
+    def run(self, edit):
+        if not self.view.sel():
+            return
+
+        self.view.window().show_quick_panel(brackets, on_select=self.on_select)
+        self.edit = edit
+
+    def on_select(self, option):
+        if option < 0:
+            return
+
+        self.view.run_command("change_brackets_at", {"bracket": brackets[option]})
+
+class ChangeBracketsAtCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit, bracket):
+        if not self.view.sel():
+            return
+
+        for sel in list(self.view.sel()):
+            a, b = sorted(sel.to_tuple())
+            b1 = sublime.Region(a - 1, a)
+            b2 = sublime.Region(b, b + 1)
+            br = self.view.substr(b1) + self.view.substr(b2)
+            if br not in brackets:
+                continue
+
+            self.view.replace(edit, b1, bracket[0])
+            self.view.replace(edit, b2, bracket[1])
